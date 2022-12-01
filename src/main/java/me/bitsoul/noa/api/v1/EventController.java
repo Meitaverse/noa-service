e82cecb7af91d1e5dad84e91e0069cfd66f31666
liujh@beautifulreading.com <travis@travis-ci.org>
2022-12-01 10:02:07 +0800

    fix bug

--- src/main/java/me/bitsoul/noa/api/v1/EventController.java
+++ src/main/java/me/bitsoul/noa/api/v1/EventController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1")
+@RestController("/v1/")
 public class EventController {
     @Autowired
     private EventService eventService;
