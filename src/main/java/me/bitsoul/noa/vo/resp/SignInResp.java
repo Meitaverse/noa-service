1fd0c5c0143b866eccb8bc1d0a07bbeb66587d7b
Manny <13724847883@163.com>
2022-12-05 14:07:42 +0800

    better

--- src/main/java/me/bitsoul/noa/vo/resp/SignInResp.java
+++ src/main/java/me/bitsoul/noa/vo/resp/SignInResp.java
@@ -1,15 +1,19 @@
 package me.bitsoul.noa.vo.resp;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * @author lxbang
  * @create 2022/12/2 4:51 下午
  */
+@ApiModel
 @Data
 public class SignInResp extends BaseResp {
 
+    @ApiModelProperty(name = "请求凭证",required = true)
     @JsonProperty("jwt")
     private String jwt;
 
